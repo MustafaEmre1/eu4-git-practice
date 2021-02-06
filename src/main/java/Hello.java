cf8f3100dd343431c9392a3677ca3c4bedb36f6a
MustafaEmre1 <mstfemre51@gmail.com>
2021-02-06 22:15:26 +0300

    third commit from intellij

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("eu4 best");
+        System.out.println("third");
+
+
     }
 }
